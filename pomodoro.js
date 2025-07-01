33345e9f1fb3372498c3a19d06176d3b5dd867bf
Dfsmist <dfsmister@gmail.com>
2025-07-01 11:40:39 +0300

    Start/stop

--- pomodoro.js
+++ pomodoro.js
@@ -1,9 +1,10 @@
 let time = 0;
 let start_timer = null;
+let isRunning = false;
 
 function updateDisplay() {
-    const min = document.querySelector(".min");
-    const sec = document.querySelector(".sec");
+    const min = document.querySelector('.min');
+    const sec = document.querySelector('.sec');
 
     const time_min = Math.floor(time / 60);
     const time_sec = time - time_min * 60;
@@ -21,24 +22,41 @@ function updateDisplay() {
 }
 
 function timer() {
-    const start_button = document.querySelector(".start");
+    const start_button = document.querySelector('.start');
+    const stop_button = document.querySelector('.stop');
+    stop_button.disabled = true;
     start_button.addEventListener('click', function() {
-        const min = document.querySelector(".min");
-        const sec = document.querySelector(".sec");
-        time = parseInt(min.innerHTML) * 60 + parseInt(sec.innerHTML);
-        start_button.innerHTML = "Пауза";
-        start_timer = setInterval(() => {
-            time--;
-            if (time <= 0) {
+        if (!isRunning) {
+            isRunning = true;
+            const min = document.querySelector('.min');
+            const sec = document.querySelector('.sec');
+            time = parseInt(min.innerHTML) * 60 + parseInt(sec.innerHTML);
+            start_button.innerHTML = "Пауза";
+            start_timer = setInterval(() => {
+                time--;
+                if (time <= 0) {
+                    clearInterval(start_timer);
+                    alert("Время вышло!");
+                }
+                updateDisplay();
+            }, 1000);
+            stop_button.disabled = false;
+            stop_button.addEventListener('click', function() {
                 clearInterval(start_timer);
-                alert("Время вышло!");
-            }
-            updateDisplay();
-        }, 1000);
+                isRunning = false;
+                min.innerHTML = "25";
+                sec.innerHTML = "00";
+                start_button.innerHTML = "Старт";
+                stop_button.disabled = true;
+            });
+        } else {
+            isRunning = false;
+            clearInterval(start_timer);
+            start_button.innerHTML = "Продолжить";
+        }
     });
 
-    // const stop_button = document.querySelector(".stop");
-    // const pomodoro_span = document.querySelector("pomodoro-span");
+    // const pomodoro_span = document.querySelector('pomodoro-span');
 }
 
 timer();
